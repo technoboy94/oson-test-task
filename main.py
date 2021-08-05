346a8bfc43b5e3af4a5452ea1a528095d056ab42
Roman Afanasev <rafanasev@astralinux.ru>
2021-08-06 02:20:27 +0300

    added solution

---
+++ main.py
@@ -0,0 +1,18 @@
+def remove_spaces(row: str):
+    row = list(row)
+    i = 0
+    for j in range(len(row)):
+        if row[i] != row[j] or row[i] == row[j] and row[j] != "_" and i != j:
+            i += 1
+            row[i] = row[j]
+    row[:] = row[:i + 1]
+    if len(row) > 1 and row[0] == "_":
+        row.pop(0)
+    if len(row) > 1 and row[-1] == "_":
+        row.pop(-1)
+    return "".join(row)
+
+
+if __name__ == "__main__":
+    example = "_On__my___home_world"
+    print(remove_spaces(example))
